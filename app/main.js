d17aae5d836ac4269d765c434501f76e3900c43c
nnnkkk12 <04gwt04@gmail.com>
2020-11-21 11:02:34 +0500

    savee

--- app/main.js
+++ app/main.js
@@ -41,9 +41,155 @@ $("#2").click(function(){
 	output();
 });
 
+$("#3").click(function(){
+	console.log(3);
+
+	if (calculator.numberA === null) {
+		calculator.numberA = 3;
+		calculator.output = 3;
+	}
+
+	else {
+		calculator.numberB = 3;
+		calculator.output = 3;
+	}
+	
+	output();
+});
+
+
+$("#4").click(function(){
+	console.log(4);
+
+	if (calculator.numberA === null) {
+		calculator.numberA = 4;
+		calculator.output = 4;
+	}
+
+	else {
+		calculator.numberB = 4;
+		calculator.output = 4;
+	}
+	
+	output();
+});
+
+
+$("#5").click(function(){
+	console.log(5);
+
+	if (calculator.numberA === null) {
+		calculator.numberA = 5;
+		calculator.output = 5;
+	}
+
+	else {
+		calculator.numberB = 5;
+		calculator.output = 5;
+	}
+	
+	output();
+});
+
+$("#6").click(function(){
+	console.log(6);
+
+	if (calculator.numberA === null) {
+		calculator.numberA = 6;
+		calculator.output = 6;
+	}
+
+	else {
+		calculator.numberB = 6;
+		calculator.output = 6;
+	}
+	
+	output();
+});
+
+$("#7").click(function(){
+	console.log(7);
+
+	if (calculator.numberA === null) {
+		calculator.numberA = 7;
+		calculator.output = 7;
+	}
+
+	else {
+		calculator.numberB = 7;
+		calculator.output = 7;
+	}
+	
+	output();
+});
+
+$("#8").click(function(){
+	console.log(8);
+
+	if (calculator.numberA === null) {
+		calculator.numberA = 8;
+		calculator.output = 8;
+	}
+
+	else {
+		calculator.numberB = 8;
+		calculator.output = 8;
+	}
+	
+	output();
+});
+
+$("#9").click(function(){
+	console.log(9);
+
+	if (calculator.numberA === null) {
+		calculator.numberA = 9;
+		calculator.output = 9;
+	}
+
+	else {
+		calculator.numberB = 9;
+		calculator.output = 9;
+	}
+	
+	output();
+});
+
+
+$("#0").click(function(){
+	console.log(0);
+
+	if (calculator.numberA === null) {
+		calculator.numberA = 0;
+		calculator.output = 0;
+	}
+
+	else {
+		calculator.numberB = 0;
+		calculator.output = 0;
+	}
+	
+	output();
+});
+
 $("#plus").click(function(){
 	calculator.func = 'plus';
 	console.log('plus');
+});
+
+$("#minus").click(function(){
+	calculator.func = 'minus';
+	console.log('minus');
+});
+
+$("#multiplication").click(function(){
+	calculator.func = 'multiplication';
+	console.log('multiplication');
+});
+
+$("#delenie").click(function(){
+	calculator.func = 'delenie';
+	console.log('delenie');
 });
 
 $("#enter").click(function(){
@@ -53,9 +199,31 @@ $("#enter").click(function(){
 		calculator.enter = calculator.numberA + calculator.numberB;
 		calculator.output = calculator.enter;
 	}
+
+	if (calculator.func === 'minus') {
+		calculator.enter = calculator.numberA - calculator.numberB;
+		calculator.output = calculator.enter;
+	}
+
+	if (calculator.func === 'multiplication') {
+		calculator.enter = calculator.numberA * calculator.numberB;
+		calculator.output = calculator.enter;
+	}
+
+	if (calculator.func === 'delenie') {
+		calculator.enter = calculator.numberA / calculator.numberB;
+		calculator.output = calculator.enter;
+	}
+
 	
+
 	output();	
+	
 });
+
+
+
+
 
 function output() {
 	$("#output").text(calculator.output);
